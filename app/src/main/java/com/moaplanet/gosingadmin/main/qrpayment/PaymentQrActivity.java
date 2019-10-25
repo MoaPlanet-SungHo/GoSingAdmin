885de77009052678623676ed829d456bbad8d05d
lhykno <lhykno@moaplanet.co.kr>
2019-10-25 14:03:58 +0900

    인트로 로그인 gone 결제 생성코드 화면 추가, 타이머 추가.

--- app/src/main/java/com/moaplanet/gosingadmin/main/qrpayment/PaymentQrActivity.java
+++ app/src/main/java/com/moaplanet/gosingadmin/main/qrpayment/PaymentQrActivity.java
@@ -1,9 +1,14 @@
 package com.moaplanet.gosingadmin.main.qrpayment;
 
 
+import android.widget.TextView;
+
 import com.moaplanet.gosingadmin.R;
 import com.moaplanet.gosingadmin.common.activity.BaseActivity;
 import com.moaplanet.gosingadmin.common.view.CommonTitleBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Qr Code 금액입력 및 생성 Activity
@@ -11,6 +16,9 @@ import com.moaplanet.gosingadmin.common.view.CommonTitleBar;
 public class PaymentQrActivity extends BaseActivity {
 
     private CommonTitleBar titleBar;
+    private TextView tvFiveTimer;
+    TimerTask timerTask;
+    Timer timer = new Timer();
 
     @Override
     public int layoutRes() {
@@ -21,10 +29,54 @@ public class PaymentQrActivity extends BaseActivity {
     public void initView() {
         titleBar = findViewById(R.id.common_inputqr_title_bar);
         titleBar.setTitle("결제 QR 코드");
+        tvFiveTimer = findViewById(R.id.show_five_minute);
+        startTimerTask();
     }
 
     @Override
     public void initListener() {
 
     }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    }
+
+    private void startTimerTask() {
+        stopTimerTask();
+
+        timerTask = new TimerTask() {
+            int count = 300;
+
+            @Override
+            public void run() {
+                count--;
+                tvFiveTimer.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        int day = count / (60 * 60 * 24);
+                        int hour = (count - day * 60 * 60 * 24) / (60 * 60);
+                        int minute = (count - day * 60 * 60 * 24 - hour * 3600) / 60;
+                        int second = count % 60;
+                        tvFiveTimer.setText("0"+minute +":"+ second);
+//                        tvFiveTimer.setText(count + " 초");
+
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 1000);
+    }
+
+    private void stopTimerTask() {
+        if (timerTask != null) {
+            tvFiveTimer.setText("60 초");
+            timerTask.cancel();
+            timerTask = null;
+        }
+    }
 }
+
