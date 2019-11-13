aa6e0a4a31735666f77e605f49bedc3400a4e992
Hk4549 <hkjw4549@gmail.com>
2019-11-13 19:38:56 +0900

    카드 리스트 불러오는 res dto 추가

--- app/src/main/java/com/moaplanet/gosingadmin/main/submenu/charge/model/dto/res/ResCardListDto.java
+++ app/src/main/java/com/moaplanet/gosingadmin/main/submenu/charge/model/dto/res/ResCardListDto.java
@@ -1,12 +1,49 @@
 package com.moaplanet.gosingadmin.main.submenu.charge.model.dto.res;
 
+import com.google.gson.annotations.SerializedName;
 import com.moaplanet.gosingadmin.network.model.CommonResDto;
+
+import java.util.List;
 
 /**
  * 카드 리스트 데이터 받을 Dto
  */
 public class ResCardListDto extends CommonResDto {
 
+    // 카드 리스트
+    @SerializedName("card_list")
+    private List<CardInformationDto> cardInformationDtoList;
 
+    /**
+     * 카드 정보를 담는 Dto
+     */
+    private class CardInformationDto {
+
+        // 카드 헤쉬
+        @SerializedName("card_hash")
+        private String mCardHash;
+
+        // 카드 이름
+        @SerializedName("card_name")
+        private String mCardName;
+
+        // 배치키
+        @SerializedName("batch_key")
+        private String mBatchKey;
+
+        // ---- getter start ----- //
+        public String getmCardHash() {
+            return mCardHash;
+        }
+
+        public String getmCardName() {
+            return mCardName;
+        }
+
+        public String getmBatchKey() {
+            return mBatchKey;
+        }
+        // ---- getter end ----- //
+    }
 
 }
